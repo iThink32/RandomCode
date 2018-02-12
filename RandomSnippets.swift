3983c11328157752610a752cd6a07cd6c33efe05
Shashank <34152717+iThink32@users.noreply.github.com>
2018-02-12 11:33:59 +0530

    Update RandomSnippets.swift

--- RandomSnippets.swift
+++ RandomSnippets.swift
@@ -1,2 +1,14 @@
 Random Code:-
 
+    class func switchResponder<T:Countable>(textField:UITextField,arrTextFields:[UITextField],enumInstance:T.Type) {
+        let nextTag = (textField.tag + 1) % enumInstance.count()
+        let nextTextFieldTag = arrTextFields.filter({ (textField) -> Bool in
+            return textField.tag == nextTag
+        }).first
+        guard textField.tag != enumInstance.count() - 1, let reqdTextField = nextTextFieldTag else{
+            textField.resignFirstResponder()
+            return
+        }
+        reqdTextField.becomeFirstResponder()
+    }
+
